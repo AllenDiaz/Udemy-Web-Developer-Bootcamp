1bad72d3f64289573bc6d4d2e3819259189bcec2
AllenDiaz <allengdiaz2019@gmail.com>
2025-03-14 13:06:23 +0800

    added the project crud comment

--- section-35-defining-restful-routes/index.js
+++ section-35-defining-restful-routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid');
+
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(express.json())
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
@@ -10,18 +14,22 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 const comments = [
     {
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog'
     },
     {
+        id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete your account'
     },
     {
+        id: uuid(),
         username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
@@ -40,9 +48,32 @@ app.get('/comments/new', (req, res) => {
     res.render('comments/new')
 });
 
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment })
+
+} );
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find( c => c.id === id);
+    res.render('comments/edit', { comment });
+});
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment ;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect(`/comments`)
+    });
+    
+
 app.post('/comments', (req, res) => { 
     const { username, comment } = req.body;
-    comments.push({username, comment})
+    comments.push({username, comment, id: uuid()});
+    
     res.redirect('/comments');
 });
 
