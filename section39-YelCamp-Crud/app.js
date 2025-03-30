b77a2c6ee0c7e28b87880ba6549a86351168accf
AllenDiaz <allengdiaz2019@gmail.com>
2025-03-30 21:19:45 +0800

    Adding lesson 427(Campground show)

--- section39-YelCamp-Crud/app.js
+++ section39-YelCamp-Crud/app.js
@@ -25,6 +25,12 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const{ id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', {campground})
+})
+
 
 app.get('/makecampground', async (req, res) => {
     const newCampground = Campground({title: 'for kids', price: 42, description: 'good for kids', location: 'Philippines'});
