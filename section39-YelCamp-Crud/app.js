1042ec2222cb5dca819a9bffc9e2372c63758c99
AllenDiaz <allengdiaz2019@gmail.com>
2025-04-01 22:44:30 +0800

    section39 adding post

--- section39-YelCamp-Crud/app.js
+++ section39-YelCamp-Crud/app.js
@@ -16,6 +16,8 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -24,6 +26,18 @@ app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', { campgrounds })
 })
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    // res.send(req.body)
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+
+} )
 
 app.get('/campgrounds/:id', async (req, res) => {
     const{ id } = req.params;
@@ -37,6 +51,7 @@ app.get('/makecampground', async (req, res) => {
     await newCampground.save();
     res.send(newCampground);
 })
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
