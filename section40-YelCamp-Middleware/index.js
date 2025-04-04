dff70b32bb526aa301258f5cb87a066be1a64492
AllenDiaz <allengdiaz2019@gmail.com>
2025-04-04 16:43:58 +0800

    adding lesson 435(middleware pratice)

--- section40-YelCamp-Middleware/index.js
+++ section40-YelCamp-Middleware/index.js
@@ -3,23 +3,29 @@ const morgan = require('morgan');
 const app = express();
 
 
-app.use(morgan('common'))
-app.use((res, req, next) => {
-    console.log('This is my first middleware')
-    return next();
+app.use(morgan('tiny'))
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
 })
+// app.use((res, req, next) => {
+//     console.log('This is my first middleware')
+//     return next();
+// })
  
-app.use((res, req, next) => {
-    console.log('This is my second middleware')
-    return next();
-})
+// app.use((res, req, next) => {
+//     console.log('This is my second middleware')
+//     return next();
+// })
 
-app.use((res, req, next) => {
-    console.log('This is my second middleware')
-    return next();
-})
+// app.use((res, req, next) => {
+//     console.log('This is my second middleware')
+//     return next();
+// })
 
 app.get('/', (req, res) => {
+    console.log(`Request time date ${req.requestTime}`)
     res.send('HOME PAGE')
 })
 
