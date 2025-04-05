cb5e2b9da16e82c2699da9d5233ad7efb0fcf913
AllenDiaz <allengdiaz2019@gmail.com>
2025-04-05 17:18:05 +0800

    adding lesson 445(Styling Campground index)

--- section41-YelCamp-Adding-Basic-Design/seeds/index.js
+++ section41-YelCamp-Adding-Basic-Design/seeds/index.js
@@ -17,9 +17,13 @@ const seedDb = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = await new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur necessitatibus praesentium nulla ipsum? Perferendis asperiores consequuntur provident maxime minus, sit id magnam assumenda vitae, beatae totam possimus harum eum! Velit?',
+            price: price
         })
         await camp.save();
     }
