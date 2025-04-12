c964d5eca8f1636c06b44d718800d4a561650378
AllenDiaz <allengdiaz2019@gmail.com>
2025-04-12 19:32:40 +0800

    adding lesson 454(more handling async function)

--- section42-YelCamp-Handling-Errors/farm.js
+++ section42-YelCamp-Handling-Errors/farm.js
@@ -22,56 +22,77 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method')) 
 
 const categories = ['fruit', 'vegetable', 'dairy']
-app.get('/products', async (req, res) => {
-    const { category } = req.query;
-    if(category) {
-        const products = await Product.find({ category })
-        res.render('products/index', { products, category})
-    } else {
-        const products = await Product.find({})
-        res.render('products/index', { products, category: 'all' })
+app.get('/products', async (req, res, next) => {
+    try {
+        const { category } = req.query;
+        if(category) {
+            const products = await Product.find({ category })
+            res.render('products/index', { products, category})
+        } else {
+            const products = await Product.find({})
+            res.render('products/index', { products, category: 'all' })
+        }
+    } catch(e) {
+        next(e);
     }
 })
 
 app.get('/products/new',(req, res) => {
-    throw new AppError('Not Allowed', 402);
+    // throw new AppError('Not Allowed', 402);
     res.render('products/new', {categories})
 })
 
 // adding or creating new object and go to created Object
-app.post('/products', async (req, res) => {
+app.post('/products', async (req, res, next) => {
     // const { name, price, category } = req.body();
-    const newProduct = new Product(req.body)
-    await newProduct.save();
-    console.log(newProduct)
-    res.redirect(`/products/${newProduct.id}`)
+    try {
+        const newProduct = new Product(req.body)
+        await newProduct.save();
+        console.log(newProduct)
+        res.redirect(`/products/${newProduct.id}`)
+    }
+    catch(e) {
+        next(e);    
+    }
 })
 
 app.get('/products/:id', async (req, res, next) => {
-    const { id } = req.params;
-    const product = await Product.findById(id)
-    if(!product) {
-       return next(new AppError('Product not found', 404));
-    }
-    else {
-        res.render('products/show', { product })
+    try{
+        const { id } = req.params;
+        const product = await Product.findById(id)
+        if(!product) {
+           throw new AppError('Product not found', 404);
+        }
+        else {
+            res.render('products/show', { product })
+        }
+    }catch(e) {
+        next(e);
     }
     // console.log(product)
 })
 
 app.get('/products/:id/edit', async (req, res, next) => {
-    const { id } = req.params;
-    const product = await Product.findById(id)
-    if(!product) {
-        return next(new AppError('Product not found', 404));
-     }
-    res.render('products/edit', { product, categories })
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id)
+        if(!product) {
+            throw new AppError('Product not found', 404);
+         }
+        res.render('products/edit', { product, categories })
+    }catch(e){
+        next(e);
+    }
 })
 
-app.put('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
-    res.redirect(`/products/${product.id}`,)
+app.put('/products/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+        res.redirect(`/products/${product.id}`,)
+    } catch(e){
+        next(e);
+    }
 })
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
