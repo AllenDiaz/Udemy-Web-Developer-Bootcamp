0d6eb05bbdc9475587487c65d1e062045ef46dc4
AllenDiaz <allengdiaz2019@gmail.com>
2025-05-22 15:18:05 +0800

    adding lesson494(Introduction cookies)

--- section47-cookies/index.js
+++ section47-cookies/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const shelterRoutes = require("./routes/shelters.js");
 const dogRoutes = require("./routes/dogs.js");
+const adminRoutes = require("./routes/admin.js");
 
 app.use("/", shelterRoutes);
 app.use("/dogs", dogRoutes);
+app.use("/admin", adminRoutes);
 
 app.listen(3000, () => {
   console.log("Serving app on localhost");
