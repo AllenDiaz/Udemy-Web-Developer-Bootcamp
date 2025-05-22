26d4a74a69634f4a2c6fb04abf4a5c94d5047c19
AllenDiaz <allengdiaz2019@gmail.com>
2025-05-22 18:50:19 +0800

    adding lesson502(Express Session)

--- section48-session-and-flash/index.js
+++ section48-session-and-flash/index.js
@@ -2,6 +2,34 @@ const express = require("express");
 const session = require("express-session");
 const app = express();
 
+app.use(
+  session({
+    secret: "thisissecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Session management is active");
+});
+
+app.get("/setsession", (req, res) => {
+  req.session.user = { name: "Allen", role: "Developer" };
+  res.send("Session data has been set!");
+});
+
+//get session
+app.get("/getsession", (req, res) => {
+  // Ensure session.user exists before accessing properties
+  const user = req.session.user || {};
+  const name = user.name || "unknown"; //if this false get value
+  const role = user.role || "unknown";
+
+  res.send(`So you are ${name} and you are a ${role} `);
+});
+
 app.listen("3000", () => {
   console.log("Localhost run on 3000");
 });
