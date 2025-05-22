533b5208398465a988deb84d06b19267818c2d40
AllenDiaz <allengdiaz2019@gmail.com>
2025-05-22 17:38:04 +0800

    adding lesson498(Signing Cookie)

--- section47-cookies/index.js
+++ section47-cookies/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(cookieParser());
+app.use(cookieParser("thisismysecret"));
 
 app.get("/greet", (req, res) => {
   const { name = "No-name" } = req.cookies;
@@ -14,7 +14,17 @@ app.get("/setname", (req, res) => {
   res.cookie("animal", "Dog");
   res.send("Allen Diaz");
 });
-app.get("/setname", (req, res) => {});
+
+app.get("/getsignedcookie", (req, res) => {
+  res.cookie("fruit", "grape", { signed: true });
+  res.send("You signed your fruit cookie");
+});
+
+app.get("/verifyfruit", (req, res) => {
+  const cookies = req.cookies;
+  const fruit = req.signedCookies;
+  res.send(fruit);
+});
 app.listen("3000", () => {
   console.log("Localhost run on 3000");
 });
