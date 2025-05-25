2fe17af4faa5b58671f3c0ed9b8432d9a4598559
AllenDiaz <allengdiaz2019@gmail.com>
2025-05-25 18:31:32 +0800

    adding lesson518(Auth Demo Registering)

--- section50-Authentication-from-Scratch/index.js
+++ section50-Authentication-from-Scratch/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const app = express();
-const user = require("./models/user");
+const User = require("./models/user");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/auth");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("This is the homepage");
+});
 
 app.get("/register", (req, res) => {
   res.render("register");
@@ -11,6 +27,17 @@ app.get("/register", (req, res) => {
 
 app.get("/secret", (req, res) => {
   res.send("THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN! ");
+});
+
+app.post("/register", async (req, res) => {
+  const { password, username } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
