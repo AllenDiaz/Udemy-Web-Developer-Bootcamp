eea580cf0cfdbddaa441891662ee88e68bce0fa0
AllenDiaz <allengdiaz2019@gmail.com>
2025-06-15 15:43:01 +0800

    Add lesson569(Working with GeoJSON)

--- section55-Yelpcamp/controllers/campgrounds.js
+++ section55-Yelpcamp/controllers/campgrounds.js
@@ -1,6 +1,9 @@
 const Campground = require("../models/campground.js");
 const { cloudinary } = require("../cloudinary");
 
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
+
 module.exports.index = async (req, res) => {
   // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
   const campgrounds = await Campground.find({});
@@ -12,7 +15,13 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await maptilerClient.geocoding.forward(
+    req.body.campground.location,
+    { limit: 1 }
+  );
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.features[0].geometry;
+
   campground.images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
@@ -59,7 +68,11 @@ module.exports.updateCampground = async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
-  console.log(req.body.deleteImages);
+  const geoData = await maptilerClient.geocoding.forward(
+    req.body.campground.location,
+    { limit: 1 }
+  );
+  campground.geometry = geoData.features[0].geometry;
   const imgs = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
