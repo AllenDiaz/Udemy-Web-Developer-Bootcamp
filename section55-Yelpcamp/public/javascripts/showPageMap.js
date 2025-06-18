1d2e31a4a57f7dfbae64fd20af0dfcfd845f21bb
AllenDiaz <allengdiaz2019@gmail.com>
2025-06-18 12:02:33 +0800

    Add lesson573(Customizing Map Popup)

--- section55-Yelpcamp/public/javascripts/showPageMap.js
+++ section55-Yelpcamp/public/javascripts/showPageMap.js
@@ -4,9 +4,14 @@ const map = new maptilersdk.Map({
   container: "map",
   style: maptilersdk.MapStyle.BRIGHT,
   center: campground.geometry.coordinates, // starting position [lng, lat]
-  zoom: 20, // starting zoomx`
+  zoom: 10, // starting zoom
 });
 
 new maptilersdk.Marker()
-  .setLngLat(120.94959776848555, 15.33177253047031)
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new maptilersdk.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
+    )
+  )
   .addTo(map);
